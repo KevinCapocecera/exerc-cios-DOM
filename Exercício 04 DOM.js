b95d9a1bacc049f55001448c80e85e2f4af5eb82
Kevin Capocecera <kevin_pereira1999@hotmail.com>
2024-08-14 21:49:28 -0300

    Add um li na ul

--- Exercício 04 DOM.js
+++ Exercício 04 DOM.js
@@ -5,15 +5,15 @@ function add() {
     nome = document.getElementById("nome").value;
     tel = document.getElementById("telefone").value;
 
-    document.getElementById("list").innerHTML += ("<br>" + nome + " - " + tel );
+    document.getElementById("list").appendChild(document.createElement("li")).innerHTML += (nome + " - " + tel);
 
     document.getElementById("nome").value = "";
     document.getElementById("telefone").value = "";
 
     document.getElementById("nome").focus();
-}   
+}
 
 function remover() {
-   var rem = document.getElementById("list");
-   rem.removeChild(rem.lastChild);
+    var rem = document.getElementById("list");
+    rem.removeChild(rem.lastChild);
 }
