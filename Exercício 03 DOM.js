6be8a393d1562297546d6a1c55da6fbbeb54d9d9
Kevin Capocecera <kevin_pereira1999@hotmail.com>
2024-08-14 22:10:51 -0300

    Exibir os multiplos ao inves de exibir somente o contador

--- Exercício 03 DOM.js
+++ Exercício 03 DOM.js
@@ -1,22 +1,23 @@
 var min = 0
 var max = 0
 var result = 0
+var num = 0
 
 function contar() {
     min = parseInt(document.getElementById("min").value);
     max = parseInt(document.getElementById("max").value);
-    console.log(min);
-    console.log(max);
 
     if (min > max) {
         alert("O minimo tem que ser menor que o maximo");
     } else {
         for (var i = min; i <= max; i++) {
             if (i % 2 == 0 && i % 3 == 0) {
+                num = i
                 result = result + 1
-                document.getElementById("result").innerHTML = result
+                document.getElementById("result").innerHTML = ("Existem " + result + " multiplos de 2 e 3, que são: " + "<br>");
+                document.getElementById("resultado").innerHTML += (num + "<br>");
             }
-
+            
         }
         result = 0
         document.getElementById("min").value = "";
